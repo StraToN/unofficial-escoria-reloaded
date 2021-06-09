3dfa2f134fd3285aa2f9cd1dec3e2463b3a086cf
Dennis Ploeger <develop@dieploegers.de>
2021-06-09 20:30:43 +0200

    feat: Optimized ESCInventoryItem

--- addons/escoria-core/game/core-scripts/escinventoryitem.gd
+++ addons/escoria-core/game/core-scripts/escinventoryitem.gd
@@ -1,23 +1,57 @@
+# An Inventory item in an ESC game
 extends TextureButton
 class_name ESCInventoryItem
 
-func get_class():
-	return "ESCInventoryItem"
 
-export(String) var global_id
-
+# Signal emitted when the item was left clicked
+#
+# #### Parameters
+#
+# - item_id: Global ID of the clicked item
 signal mouse_left_inventory_item(item_id)
+
+# Signal emitted when the item was right clicked
+#
+# #### Parameters
+#
+# - item_id: Global ID of the clicked item
 signal mouse_right_inventory_item(item_id)
+
+# Signal emitted when the item was double clicked
+#
+# #### Parameters
+#
+# - item_id: Global ID of the clicked item
 signal mouse_double_left_inventory_item(item_id)
+
+# Signal emitted when the item was focused
+#
+# #### Parameters
+#
+# - item_id: Global ID of the clicked item
 signal inventory_item_focused(item_id)
+
+# Signal emitted when the item is not focused anymore
 signal inventory_item_unfocused()
 
 
+
+# The global id of this item
+export(String) var global_id
+
+
+# Connect input handlers
 func _ready():
 	connect("gui_input", self, "_on_inventory_item_gui_input")
 	connect("mouse_entered", self, "_on_inventory_item_mouse_enter")
 	connect("mouse_exited", self, "_on_inventory_item_mouse_exit")
 
+
+# Handle the gui input and emit the respective signals
+#
+# #### Parameters
+#
+# - event: The event received
 func _on_inventory_item_gui_input(event : InputEvent):
 	if event.is_action_pressed("switch_action_verb"):
 		if event.button_index == BUTTON_WHEEL_UP:
@@ -35,11 +69,13 @@ func _on_inventory_item_gui_input(event : InputEvent):
 					emit_signal("mouse_left_inventory_item", global_id, event)
 				if event.button_index == BUTTON_RIGHT:
 					emit_signal("mouse_right_inventory_item", global_id, event)
-	
+
+
+# Handle mouse entering the item and send the respecitve signal
 func _on_inventory_item_mouse_enter():
-	# Notify UI that item is focused (room.game.ui.Label UI)
 	emit_signal("inventory_item_focused", global_id)
 
+
+# Handle mouse leaving the item and send the respecitve signal
 func _on_inventory_item_mouse_exit():
-	# Notify UI that item is unfocused (room.game.ui.Label UI)
 	emit_signal("inventory_item_unfocused")
